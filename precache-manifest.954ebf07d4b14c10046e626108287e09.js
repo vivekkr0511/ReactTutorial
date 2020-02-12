b1683db5ae6245e24ae331b3471720eedd846674
vivekkr0511 <vivekkr0511@gmail.com>
2020-02-12 03:31:59 -0500

    Updates

---
+++ precache-manifest.954ebf07d4b14c10046e626108287e09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2954e122e0dcf2604bd63be4a215854d",
+    "url": "/ReactTutorial/index.html"
+  },
+  {
+    "revision": "d2bca46a73a72d765647",
+    "url": "/ReactTutorial/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b3c46d1e4797d8133204",
+    "url": "/ReactTutorial/static/js/2.c214d450.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ReactTutorial/static/js/2.c214d450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bca46a73a72d765647",
+    "url": "/ReactTutorial/static/js/main.f750d45d.chunk.js"
+  },
+  {
+    "revision": "02d9d15f47b991e13daf",
+    "url": "/ReactTutorial/static/js/runtime-main.94a0d3c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactTutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
